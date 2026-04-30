05068475bda53335934380cf1f06473a3ddd9107
bhav380-2 <bhaweshrathour18@gmail.com>
2026-04-30 08:16:15 +0530

    Factory Method Pattern

---
+++ factory/pizzasfm/NYStyleCheesePizza.java
@@ -0,0 +1,11 @@
+package factory.pizzasfm;
+
+public class NYStyleCheesePizza extends Pizza{
+
+    public NYStyleCheesePizza(){
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "thin crust dough";
+        sauce = "Marinara sauce";
+        toppings.add("Grated Reggiano Cheese");
+    }
+}
---
+++ factory/pizzasfm/PizzaTest.java
@@ -0,0 +1,43 @@
+package factory.pizzasfm;
+
+// Factory Method Pattern - Defines an interface to create an object but lets subclasses decide
+// which class to instantiate. Factory Method lets a class to defer instantiation to the subclasses.
+
+// Design Principles:-
+//1. Encapsulate what varies
+//2. Program to interface not implementation
+//3. Favor composition over inheritance
+//4. Strive for loosely coupled designs between objects that interact with each other
+//5. Systems should be open for extension but closed for modification
+//6. Depend on abstraction. Do not depend on concrete classes
+
+public class PizzaTest{
+    public static void main(String [] args){
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoPizzaStore();
+
+        Pizza pizza = nyStore.orderPizza("cheese");
+        System.out.println("Ethan ordered a "+pizza.getName()+"\n");
+
+        pizza = chicagoStore.orderPizza("cheese");
+        System.out.println("Joel ordered a "+pizza.getName()+"\n");
+
+        pizza = nyStore.orderPizza("clam");
+        System.out.println("Ethan ordered a "+pizza.getName()+"\n");
+
+
+		pizza = chicagoStore.orderPizza("clam");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+
+		pizza = nyStore.orderPizza("pepperoni");
+		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+ 
+		pizza = chicagoStore.orderPizza("pepperoni");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+
+		pizza = nyStore.orderPizza("veggie");
+		System.out.println("Ethan ordered a " + pizza.getName() + "\n");
+		pizza = chicagoStore.orderPizza("veggie");
+		System.out.println("Joel ordered a " + pizza.getName() + "\n");
+    }
+}
