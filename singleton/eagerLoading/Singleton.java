5ffa98ef180eda41736ba184cc9cf7b6f273af75
bhav380-2 <bhaweshrathour18@gmail.com>
2026-05-05 17:48:24 +0530

    thread safe singleton using eager loading

---
+++ singleton/eagerLoading/Singleton.java
@@ -0,0 +1,17 @@
+package singleton.eagerLoading;
+
+public class Singleton{
+    private static Singleton uniqueInstance = new Singleton();
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        return uniqueInstance;
+    }
+
+    public String getDescription(){
+        return "I'm a eagerly initialized Singleton!";
+    }
+}
